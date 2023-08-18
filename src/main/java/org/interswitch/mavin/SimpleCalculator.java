be073f95592287f416ede4369a56972aa637d626
Oluwadamilola Olatunji <Oluwadamilola.Ola@Interswitchng.com>
2023-08-18 12:31:33 +0100

    Add other file

---
+++ src/main/java/org/interswitch/mavin/SimpleCalculator.java
@@ -0,0 +1,34 @@
+package org.interswitch.mavin;
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    private double num1;
+    private double num2;
+
+    public SimpleCalculator(double num1, double num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public double add() {
+        return num1 + num2;
+    }
+
+    public double subtract() {
+        return num1 - num2;
+    }
+
+    public double multiply() {
+        return num1 * num2;
+    }
+
+    public double divide() {
+        if (num2 != 0) {
+            return num1 / num2;
+        } else {
+            throw new ArithmeticException("Cannot divide by zero.");
+        }
+    }
+
+}
